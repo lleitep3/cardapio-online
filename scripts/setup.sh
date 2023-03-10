2cb39c3ddc2d1ceec46a0e7dd63c7a8ccd98922f
Leandro Leite <leandro@leandroleite.info>
2023-03-10 12:44:02 -0300

    feat: web project

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-rm ./packages/nginx/ssl/* -rf
+rm ./packages/proxy/nginx/ssl/* -rf
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./packages/nginx/ssl/web.key -out ./packages/nginx/ssl/web.crt -subj "/C=BR/ST=SP/L=São Paulo/O=Menu Online/OU=lleitep3/CN=example.com"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./packages/proxy/nginx/ssl/web.key -out ./packages/proxy/nginx/ssl/web.crt -subj "/C=BR/ST=SP/L=São Paulo/O=Menu Online/OU=lleitep3/CN=cardapio.local"
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./packages/nginx/ssl/server.key -out ./packages/nginx/ssl/server.crt -subj "/C=BR/ST=SP/L=São Paulo/O=Menu Online/OU=lleitep3/CN=example.com"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./packages/proxy/nginx/ssl/server.key -out ./packages/proxy/nginx/ssl/server.crt -subj "/C=BR/ST=SP/L=São Paulo/O=Menu Online/OU=lleitep3/CN=cardapio.local"
 

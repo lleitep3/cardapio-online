702b8b782ec5102e777a955a3cc78697802a67fc
Leandro Leite <leandro@leandroleite.info>
2023-03-14 10:44:37 -0300

    ci: test:api

--- packages/api/src/config.ts
+++ packages/api/src/config.ts
@@ -1,6 +1,6 @@
 
 export const config = {
-  appName: process.env.APP_NAME || 'no name',
+  appName: process.env.APP_NAME || 'NO NAME',
   port: +(process.env.PORT || 5000),
   env: process.env.NODE_ENV || 'development',
 }

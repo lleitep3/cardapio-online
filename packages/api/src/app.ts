595e8a9ccef6c88bb22c882dc2e519fe886d315e
Leandro Leite <leandro@leandroleite.info>
2023-03-14 10:35:58 -0300

    ci: tests

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -10,7 +10,13 @@ app.get('/', (req, res) => {
 app.get('/healthcheck', (req, res) => {
   res.setHeader('Content-Type', 'application/json')
   res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate')
-  res.send({ status: 'ok' })
+  res.send({
+    status: 'ok',
+    env: config.env,
+    appName: config.appName,
+    currentTime: new Date().toISOString(),
+    version: process.env.npm_package_version,
+  })
 })
 
 app.listen(config.port, '0.0.0.0', () => {
